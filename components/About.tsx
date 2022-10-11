ba3585bd4f1ee77c2a26f6e1c14eba7ab13caa1b
alejandro-dominguez <alejandro.dominguez1@live.com>
2022-10-11 17:18:12 -0300

    animations performance

--- components/About.tsx
+++ components/About.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Image from 'next/image';
-import { motion } from "framer-motion";
 import DevAvatar from '../images/dev-avatar.png';
 
 export default function About() {
@@ -10,49 +9,22 @@ export default function About() {
 			Sobre mí
 		</h3>
 		<div className="flex flex-col md:flex-row items-start justify-center mt-0 md:mt-4">
-			<motion.div className='relative h-[7.5rem] w-[7.5rem] md:h-60 md:w-60 mt-28 md:mt-0 mx-auto flex-shrink-0'
-				initial={{
-					opacity: 0,
-					x: -150
-				}}
-				whileInView={{
-					opacity: 1,
-					x: 0
-				}}
-				viewport={{
-					once: true
-				}}
-				transition={{
-					duration: 1
-				}}>
+			<div className='relative h-[7.5rem] w-[7.5rem] md:h-60 md:w-60 mt-28 md:mt-0 mx-auto flex-shrink-0'>
 				<Image src={DevAvatar} alt="dev avatar" layout='fill' objectFit='contain' className='aboutImg'/>
-			</motion.div>
-			<motion.div className="flex flex-col max-w-xl items-center md:items-start gap-2 md:gap-6 pl-4 pr-4
-				md:pl-0 mt-4 md:mt-0"
-				initial={{
-					opacity: 0,
-					x: 150
-				}}
-				whileInView={{
-					opacity: 1,
-					x: 0
-				}}
-				viewport={{
-					once: true
-				}}
-				transition={{
-					duration: 1
-				}}>
+			</div>
+			<div className="flex flex-col max-w-xl items-center md:items-start gap-2 md:gap-6 pl-4 pr-4
+				md:pl-0 mt-4 md:mt-0">
 				<h4 className='text-xl md:text-3xl text-center md:text-left font-semibold'>Un poco de información
 					<span className='text-[#ffe500]'> sobre mí</span>
 				</h4>
 				<p className='text-sm md:text-base'>Soy un apasionado de la programación y el diseño. Vengo del 
 					rubro del diseño gráfico y el marketing digital. Siempre había querido programar páginas web, 
 					hasta que comencé a hacer y desde ese momento no he parado de aprender nuevas habilidades que 
-					verdaderamente me fascinan.<span className='my-2 block' />Actualmente mi especialidad es el desarrollo front end, es el área 
-					que más me interesa por su relación con todo el mundo del diseño.
+					verdaderamente me fascinan.<span className='my-2 block' />Actualmente mi especialidad es el 
+					desarrollo front end, es el área que más me interesa por su relación con todo el mundo del 
+					diseño.
 				</p>
-			</motion.div>
+			</div>
 		</div>
 	</div>
 	)
